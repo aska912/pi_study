dfb4313a355d3a5e37d304a53eb9bc3ef0242dc9
pi3 <pi3@raspi3.com>
2021-02-12 13:25:17 +0800

    On branch main Your branch is up to date with 'origin/main'. Changes to be committed: 	new file:   TM1637.py 	new file:   four_digital_led.py
    新增TM1637驱动和4位数码管的应用程序

---
+++ TM1637.py
@@ -0,0 +1,123 @@
+#encoding:utf-8
+
+import RPi.GPIO as GPIO
+from time import sleep
+
+
+Hight = 1
+Low   = 0
+
+PULSE_WIDTH = {
+    '1/16':   0x0, #最暗
+    '2/16':   0x1,
+    '4/16':   0x2,
+    '10/16':  0x3,
+    '11/16':  0x4,
+    '12/16':  0x5,
+    '13/16':  0x6,
+    '14/16':  0x7, #最暗
+}
+
+class TM1637:
+    def __init__(self, clk_pin, dio_pin):
+        self.__clk = clk_pin
+        self.__dio = dio_pin
+
+    @property
+    def CLK(self):
+        return self.__clk
+
+    @property
+    def DIO(self):
+        return self.__dio
+
+    @property
+    def GRID1(self):
+        return 0xc0
+
+    @property
+    def GRID2(self):
+        return 0xc1
+
+    @property
+    def GRID3(self):
+        return 0xc2
+
+    @property
+    def GRID4(self):
+        return 0xc3
+
+    @property
+    def GRID5(self):
+        return 0xc4
+
+    @property
+    def GRID6(self):
+        return 0xc6
+
+    def init(self):
+        GPIO.setup(self.__clk, GPIO.OUT)
+        GPIO.setup(self.__dio, GPIO.OUT)
+
+    def set_mode_as_display(self):
+        self.__send_cfg_cmd(0x40)
+
+    def set_mode_as_keyscan(self):
+        self.__send_cfg_cmd(0x42)
+
+    def set_addr_as_fixed(self):
+        self.__send_cfg_cmd(0x44)
+
+    def set_addr_as_auto(self):
+        pass
+        #self.__send_cfg_cmd(0x40)
+
+    def turn_off_led(self):
+        self.__send_cfg_cmd(0x80)
+
+    def turn_on_led(self, pulse_width=0x0):
+        self.__send_cfg_cmd( (0x88 | pulse_width) )
+
+    def write_data_to_addr(self, addr, data):
+        self.__start()
+        self.__write_byte(addr)
+        self.__write_byte(data)
+        self.__stop()
+
+    def __write_byte(self, data):
+        # DIO写8个bit
+        for i in range(0, 8):
+            GPIO.output(self.__clk, Low)
+            if (data >> i) & 0x01:
+                GPIO.output(self.__dio, Hight)
+            else:
+                GPIO.output(self.__dio, Low)
+            GPIO.output(self.__clk, Hight)
+
+        # DIO写完一个byte，等待ACK
+        GPIO.output(self.__clk, Low)
+        GPIO.output(self.__dio, Hight)
+        GPIO.output(self.__clk, Hight)
+        GPIO.setup(self.__dio, GPIO.IN)
+        while( GPIO.input(self.__dio) ):
+            pass
+        GPIO.setup(self.__dio, GPIO.OUT)
+
+    def __send_cfg_cmd(self, cmd):
+        self.__start()
+        self.__write_byte(cmd)
+        self.__stop()
+
+    def __start(self):
+        GPIO.output(self.__clk, Hight) #时钟打高电平
+        GPIO.output(self.__dio, Hight)
+        GPIO.output(self.__dio, Low)
+        GPIO.output(self.__clk, Low) #时钟打低电平
+
+    def __stop(self):
+        GPIO.output(self.__dio, Low) 
+        GPIO.output(self.__clk, Low)
+        GPIO.output(self.__clk, Hight)
+        GPIO.output(self.__dio, Hight) 
+        
+    
---
+++ four_digital_led.py
@@ -0,0 +1,166 @@
+#encoding:utf-8
+
+import TM1637
+
+#------------------------------------------------------------------------
+#               
+#          a      Hight                              Low
+#       __/       dp   g    f    e    d    c    b    a
+#   g_f|__|b |    SG8  SG7  SG6  SG5  SG4  SG3  SG2  SG1
+#     e|__|c |    1    0    1    1    1    1    1    1   0xBF ==> '0:'
+#        |    \
+#        d     dp
+#
+#------------------------------------------------------------------------
+LED_FONTS = {
+    '0':   0x3f,
+    '1':   0x06,
+    '2':   0x5b,
+    '3':   0x4f,
+    '4':   0x66,
+    '5':   0x6d,
+    '6':   0x7d,
+    '7':   0x07,
+    '8':   0x7f,
+    '9':   0x6f,
+    '0:':  0xbf,
+    '1:':  0x86,
+    '2:':  0xdb,
+    '3:':  0xcf,
+    '4:':  0xe6,
+    '5:':  0xed,
+    '6:':  0xfd,
+    '7:':  0x87,
+    '8:':  0xff,
+    '9:':  0xef,
+    'a':   0x77,
+    'c':   0x39,
+    'o':   0x63,
+    'blank': 0x00,
+}
+
+BRIGHTNESS_ADJUST_RANGE = (TM1637.PULSE_WIDTH['1/16'],   \
+                           TM1637.PULSE_WIDTH['2/16'],   \
+                           TM1637.PULSE_WIDTH['4/16'],   \
+                           TM1637.PULSE_WIDTH['10/16'],   \
+                           TM1637.PULSE_WIDTH['11/16'],   \
+                           TM1637.PULSE_WIDTH['12/16'],   \
+                           TM1637.PULSE_WIDTH['13/16'],   \
+                           TM1637.PULSE_WIDTH['14/16'])
+
+MAX_BRIGHTNRSS = 5
+MID_BRIGHTNRSS = 2
+MIN_BRIGHTNRSS = 0
+
+
+class four_digital_led:
+    def __init__(self, clk_pin, dio_pin):
+        self.tm1637 = TM1637.TM1637(clk_pin, dio_pin)
+        self.tm1637.init()
+        self.brightness = BRIGHTNESS_ADJUST_RANGE[MID_BRIGHTNRSS]
+        self.__data_buff = None
+        self.__grid_list = (self.tm1637.GRID1, \
+                            self.tm1637.GRID2, \
+                            self.tm1637.GRID3, \
+                            self.tm1637.GRID4)
+
+    def adjust_brightness(self, brightness):
+        if brightness <= 0:
+            self.brightness = MIN_BRIGHTNRSS
+        elif brightness >= len(BRIGHTNESS_ADJUST_RANGE):
+            self.brightness = MAX_BRIGHTNRSS
+        else:
+            self.brightness = brightness
+
+    def write_data(self, data_1, data_2, data_3, data_4):
+        self.__data_buff = ( (self.__grid_list[0], data_1),   \
+                             (self.__grid_list[1], data_2),   \
+                             (self.__grid_list[2], data_3),   \
+                             (self.__grid_list[3], data_4) )
+
+    def display(self):
+        if self.__data_buff is not None:
+            self.tm1637.set_mode_as_display()
+            self.tm1637.set_addr_as_fixed()
+            for addr_and_data in self.__data_buff:
+                if len(addr_and_data):
+                    addr = addr_and_data[0]
+                    data = addr_and_data[1]
+                    self.tm1637.write_data_to_addr(addr, data)
+                else:
+                    continue
+            self.tm1637.turn_on_led(self.brightness)
+        del self.__data_buff
+        self.__data_buff = None
+    
+
+
+if __name__ == "__main__":
+    import RPi.GPIO as GPIO
+    import time
+    
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BOARD)
+
+    clk = 16
+    dio = 18
+    delay = 6
+
+    led = four_digital_led(clk, dio)
+    led.adjust_brightness(MIN_BRIGHTNRSS)
+    while True:
+
+        dp = 0
+        for cnt in range(15):
+            now_time=time.localtime(time.time())
+            time_mon  = now_time[1]
+            time_mday = now_time[2]
+            time_hour = now_time[3]
+            time_min  = now_time[4]
+            #time_sec  = now_time[5]
+            hour_ge  = time_hour % 10
+            hour_shi = int(time_hour / 10)
+            min_ge   = time_min % 10
+            min_shi  = int(time_min / 10)
+            if hour_shi:
+                hour_shi_led_font = LED_FONTS['%d'%hour_shi]
+            else:
+                hour_shi_led_font = LED_FONTS['blank']
+            dp = dp ^ 1
+            if dp:
+                led.write_data(hour_shi_led_font, LED_FONTS['%d:'%hour_ge], LED_FONTS['%d'%min_shi], LED_FONTS['%d'%min_ge])
+            else:
+                led.write_data(hour_shi_led_font, LED_FONTS['%d'%hour_ge], LED_FONTS['%d'%min_shi], LED_FONTS['%d'%min_ge])
+            led.display()
+            time.sleep(1)
+
+        mon_shi  = int(time_mon / 10)
+        mon_ge   = time_mon % 10
+        mday_shi = int(time_mday / 10)
+        mday_ge  = time_mday % 10
+        if mon_shi:
+            mon_shi_led_font = LED_FONTS['%d'%mon_shi]
+        else:
+            mon_shi_led_font = LED_FONTS['blank']
+    
+        if mday_shi:
+            mday_shi_led_font = LED_FONTS['%d'%mday_shi]
+        else:
+            mday_shi_led_font = LED_FONTS['blank']
+        led.write_data(mon_shi_led_font, LED_FONTS['%d'%mon_ge], mday_shi_led_font, LED_FONTS['%d'%mday_ge])
+        led.display()
+        time.sleep(delay)
+
+        with open('/sys/class/thermal/thermal_zone0/temp', 'r') as fn:
+            cpu_temp = int(float(fn.read()) /1000)
+        #print("CPU Tempture: ", cpu_temp, '\'C')
+        cpu_temp_shi = cpu_temp / 10
+        cpu_temp_ge  = cpu_temp % 10
+        led.write_data(LED_FONTS['%d'%cpu_temp_shi], LED_FONTS['%d'%cpu_temp_ge], LED_FONTS['o'], LED_FONTS['c'])
+        led.display()
+        time.sleep(delay)
+
+        
+
+
+
